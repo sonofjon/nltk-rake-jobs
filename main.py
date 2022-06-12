80f43da8bade8df9a830370f197cd081ba8e4f7b
Andreas Jonsson <ajdev8@gmail.com>
2022-06-12 23:15:15 +0200

    Add an example job ad
    Use various element selection techniques:
    * CCS selector
    * Search
    * XPath

--- main.py
+++ main.py
@@ -4,18 +4,20 @@ from rake_nltk import Rake
 
 def extract_text():
     s = HTMLSession()
-    url = 'https://www.musicradar.com/reviews/tech/akg-c214-172209'
-    response = s.get(url)
-    return response.html.find('div#article-body', first=True).text
+    url = 'https://career.odevo.com/jobs/1652504-linux-sys-admin'
+    r = s.get(url)
+    # Select an element with a CSS selector
+    # return r.html.find('body > main > section > div.mx-auto.prose.font-company-body.overflow-hidden', first=True).text
+    # Select an element with search
+    return r.html.find('div', containing='Zabbix', first=True).text
+    # Select an element with XPath
+    # return r.html.xpath('/html/body/main/section[2]/div', first=True).text
 
+
+print(extract_text())
 
 r = Rake()
 r.extract_keywords_from_text(extract_text())
 for rating, keyword in r.get_ranked_phrases_with_scores():
     if rating > 5:
         print(rating, keyword)
-
-
-
-
-
