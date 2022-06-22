8c5f633b67bf06f4cf021d4a0f17a80a1d8f363f
Andreas Jonsson <ajdev8@gmail.com>
2022-06-22 12:37:54 +0200

    Add main functionality

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
 from requests_html import HTMLSession
 from rake_nltk import Rake
 
+import sys, getopt
+
+def main(argv):
+   url = ''
+   keyword = ''
+   try:
+      opts, args = getopt.getopt(argv,"hu:k:",["url=","keyword="])
+   except getopt.GetoptError:
+      print('main.py -u <url> -k <keyword>')
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt in ("-h", "--help"):
+         print('main.py -i <url> -o <keyword>')
+         sys.exit()
+      elif opt in ("-u", "--url"):
+         url = arg
+      elif opt in ("-k", "--keyword"):
+         keyword = arg
+   print(f'Analyzing web element matching "{keyword}" at {url}')
+
+   # print(extract_text())
+
+   # r = Rake()
+   # # r = Rake(language='swedish')
+
+   # r.extract_keywords_from_text(extract_text())
+   # for rating, keyword in r.get_ranked_phrases_with_scores():
+   #     if rating > 5:
+   #         print(round(rating), keyword)
+
+
 # English example
 def extract_text():
     s = HTMLSession()
@@ -8,18 +39,12 @@ def extract_text():
     r = s.get(url)
     # Select an element with a CSS selector
     # return r.html.find('body > main > section > div.mx-auto.prose.font-company-body.overflow-hidden', first=True).text
-    # Select an element with search
-    return r.html.find('div', containing='Zabbix', first=True).text
     # Select an element with XPath
     # return r.html.xpath('/html/body/main/section[2]/div', first=True).text
+    # Select an element with search
+    return r.html.find('div', containing='Zabbix', first=True).text
 
 
-print(extract_text())
+if __name__ == "__main__":
+   main(sys.argv[1:])
 
-r = Rake()
-# r = Rake(language='swedish')
-
-r.extract_keywords_from_text(extract_text())
-for rating, keyword in r.get_ranked_phrases_with_scores():
-    if rating > 5:
-        print(round(rating), keyword)
