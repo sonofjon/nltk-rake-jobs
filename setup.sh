a1e41257c28120202443cf677623421f87fe6cf2
Andreas Jonsson <ajdev8@gmail.com>
2022-06-25 00:26:42 +0200

    Change nltk default download directory

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
+# Run with: source setup.sh
+
 # Install requirements
 pip install -r requirements.txt
 
 # Location for NLTK data
-export NLTK_DATA=~/projects/rake-on-reviews/nltk_data
+NLTK_DATA=~/projects/nltk-rake-jobs/nltk_data
 
 # Import nltk data
-python -c "import nltk; nltk.download('punkt')"
-python -c "import nltk; nltk.download('stopwords')"
+python -c "import nltk; nltk.download('stopwords', download_dir=\"$NLTK_DATA\")"
+python -c "import nltk; nltk.download('punkt', download_dir=\"$NLTK_DATA\")"
