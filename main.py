edb955034825461961926f1edb3dc625e88f6bbf
Andreas Jonsson <ajdev8@gmail.com>
2022-06-24 23:02:28 +0200

    Use url and keyword supplied from command line

--- main.py
+++ main.py
@@ -23,30 +23,29 @@ def main(argv):
          keyword = arg
    print(f'Analyzing web element matching "{keyword}" at {url}')
 
-   # print(extract_text())
+   extract_text = extract_url_text(url, keyword)
+   print(extract_text())
 
-   # r = Rake()
-   # # r = Rake(language='swedish')
+   r = Rake()
+   # r = Rake(language='swedish')
 
-   # r.extract_keywords_from_text(extract_text())
-   # for rating, keyword in r.get_ranked_phrases_with_scores():
-   #     if rating > 5:
-   #         print(round(rating), keyword)
+   r.extract_keywords_from_text(extract_text)
+   for rating, keyword in r.get_ranked_phrases_with_scores():
+       if rating > 5:
+           print(round(rating), keyword)
 
 
 # English example
-def extract_text():
+def extract_url_text(url, keyword):
     s = HTMLSession()
-    url = 'https://career.odevo.com/jobs/1652504-linux-sys-admin'
     r = s.get(url)
     # Select an element with a CSS selector
     # return r.html.find('body > main > section > div.mx-auto.prose.font-company-body.overflow-hidden', first=True).text
     # Select an element with XPath
     # return r.html.xpath('/html/body/main/section[2]/div', first=True).text
     # Select an element with search
-    return r.html.find('div', containing='Zabbix', first=True).text
+    return r.html.find('div', containing=keyword, first=True).text
 
 
 if __name__ == "__main__":
    main(sys.argv[1:])
-
