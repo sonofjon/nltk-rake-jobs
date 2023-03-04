b760c48b735a989f8ae1556ee7da53bb95a2d54b
Andreas Jonsson <ajdev8@gmail.com>
2023-03-04 19:07:55 +0100

    Change location for data directory

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 pip install -r requirements.txt
 
 # Location for NLTK data
-export NLTK_DATA=~/projects/rake-on-reviews/nltk_data
+export NLTK_DATA=~/data/nltk_data
 
 # Import nltk data
 python -c "import nltk; nltk.download('punkt')"
