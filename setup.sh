caac7cf0d7fc02ca27a7864297af605c5c08d0ae
Andreas Jonsson <ajdev8@gmail.com>
2025-05-10 22:52:20 +0200

    setup.sh: Move data dir to central location

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 pip install -r requirements.txt
 
 # Location for NLTK data
-NLTK_DATA=~/projects/nltk-rake-jobs/nltk_data
+NLTK_DATA=~/data/nltk
 
 # Import nltk data
 python -c "import nltk; nltk.download('stopwords', download_dir=\"$NLTK_DATA\")"
